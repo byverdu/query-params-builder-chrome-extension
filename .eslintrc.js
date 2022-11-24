52339c63691a60befb2ab9de25fc7c0cee0a8894
byverdu <byverdu@gmail.com>
2022-11-24 23:06:24 +0000

    project setup

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    webextensions: true,
+  },
+  globals: {
+    bootstrap: true,
+  },
+  extends: 'eslint:recommended',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    semi: ['error', 'always'],
+  },
+};
