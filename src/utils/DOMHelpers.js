523a5077134888b6c8d8c0440cdfb407d91abbcb
Albert Vallverdu <byverdu@gmail.com>
2023-01-11 10:03:07 +0000

    refactor styling options page

--- src/utils/DOMHelpers.js
+++ src/utils/DOMHelpers.js
@@ -1,8 +1,12 @@
 function optionsTableRowBuilder(id, bundleName, urlParamKey) {
   return `
   <tr id="${id}">
-  <td class="contentEditable" data-initial-value="${bundleName}" data-value-type="bundleName" contentEditable="true">${bundleName}</td>
-  <td class="contentEditable" data-initial-value="${urlParamKey}" data-value-type="urlParamKey" contentEditable="true">${urlParamKey}</td>
+  <td class="contentEditable" data-initial-value="${bundleName}" data-value-type="bundleName" contentEditable="true">
+    <span>${bundleName}</span>
+  </td>
+  <td class="contentEditable" data-initial-value="${urlParamKey}" data-value-type="urlParamKey" contentEditable="true">
+    <span>${urlParamKey}</span>
+  </td>
   <td>
     <button data-bundle-id="${id}" class="btn btn-outline-danger delete-bundle">
       Delete
